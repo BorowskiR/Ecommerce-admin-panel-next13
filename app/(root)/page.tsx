29d9532fc180568a32d682932e0d63e13eea3ac5
Rafal <rafall.borowski@gmail.com>
2023-08-06 18:57:53 +0200

    clerk authentication

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function SetupPage() {
+  return (
+    <div className="p-4">
+      <p>This is protected root</p>
+    </div>
+  );
+}
